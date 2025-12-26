36fed080bc644078d87314661b3afefc9dbf841a
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2025-12-26 11:18:26 +0530

    add z-image lora

--- src/download_models.sh
+++ src/download_models.sh
@@ -74,6 +74,7 @@ if [ "$DOWNLOAD_ZIMAGE" == "true" ]; then
     download_hf "https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/text_encoders/qwen_3_4b.safetensors" "$TEXT_ENCODERS_DIR/qwen_3_4b.safetensors" &
     download_hf "https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/diffusion_models/z_image_turbo_bf16.safetensors" "$DIFFUSION_MODELS_DIR/z_image_turbo_bf16.safetensors" &
     download_hf "https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/vae/ae.safetensors" "$VAES_DIR/ae.safetensors" &
+    download_hf "https://huggingface.co/akkisdiary/myra-ai/resolve/main/z-image/ohmyra.safetensors" "$LORAS_DIR/ohmyra.safetensors" &
 fi
 
 # Up-Scalers
